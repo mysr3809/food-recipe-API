95d07e41dbe334a86f9d5d6d94125758dba37d09
mysr3809 <myasar0938@gmail.com>
2022-11-03 22:15:10 +0100

    some functionality and improvement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const FOOD_URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=`;
-const RECIPE_URL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
+const RECIPE_URL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const inputEl = document.querySelector('.input-group');
 
 const foodApi = async (url) => {
@@ -15,21 +15,17 @@ function getInputValue() {
   // Selecting the input element and get its value 
   const inputVal = document.getElementById("inputText").value;
   if (inputVal === '') {
-    alert('You shoud text a food')
+    alert('You shoud text a food') //there is no input throw an alert
   } else {
     // Displaying the value
-    console.log(inputVal);
+    renderMeal(inputVal);
   }
-  renderMeal(inputVal);
 }
 
 async function renderMeal(meal) {
-  const data = await foodApi(FOOD_URL + meal);
-  console.log(data);
-  const mealList = document.querySelector('.mealList');
-
-
   let col = '';
+  const data = await foodApi(FOOD_URL + meal);
+  const mealList = document.querySelector('.mealList');
   data.meals.forEach(meal => {
     col += `
           <div class="col m-2">
@@ -37,7 +33,7 @@ async function renderMeal(meal) {
               <img src="${meal.strMealThumb}" style="height:100%" class="card-img-top" alt="meal">
               <div class="card-body">
               <h4>${meal.strMeal}</h4>
-                <button type="button" onclick='showRecipe("${meal.strMeal}")' class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <button type="button" onclick='showRecipe("${meal.idMeal}")' class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 See Recipe
                 </button>
               </div>
@@ -48,3 +44,24 @@ async function renderMeal(meal) {
     mealList.innerHTML = col;
   })
 }
+
+const showRecipe = async (mealId) => {  // show instruction in the modal
+  let recipe = '';
+  let step = '';
+  const response = await fetch(RECIPE_URL + mealId); //fetc instruction of the recipe
+  const data = await response.json();
+  const recipeBox = document.getElementById('recipeBox');
+  recipe =  //all the instructions about meal
+    `
+     ${data.meals[0].strInstructions}
+    `;
+  const resultArr = recipe.split('\r\n'); // split the instructions according to recipe steps
+  resultArr.forEach(recipe => {
+    step += `
+             <p>${recipe}</p>
+            `
+  });
+  recipeBox.innerHTML = step;
+};
+
+
