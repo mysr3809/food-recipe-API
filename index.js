91dc2c2879f1e21857380873d6b17457ff399f81
mysr3809 <myasar0938@gmail.com>
2022-11-05 17:40:04 +0100

    added functionality

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const RECIPE_URL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const inputEl = document.querySelector('.input-group');
 const headerLogoEl = document.getElementById('cookerLogo');
 const startPageEl = document.querySelector('.startPage'); // hidden start page content
+const logoTextEl = document.querySelector('.logoText')
 
 
 const startPage = () => {
-  headerLogoEl.style.display = 'none';
 }
 
 
@@ -20,6 +20,7 @@ const foodApi = async (url) => {
 
 inputEl.style.width = '50%';
 function getInputValue() {
+
   // Selecting the input element and get its value 
   const inputVal = document.getElementById("inputText").value;
   if (inputVal === '') {
@@ -27,6 +28,7 @@ function getInputValue() {
   } else {
     startPageEl.style.display = 'none';
     headerLogoEl.style.display = 'block';
+    logoTextEl.style.display = 'block'
     // Displaying the value
     renderMeal(inputVal);
   }
@@ -37,13 +39,18 @@ async function renderMeal(meal) {
   const data = await foodApi(FOOD_URL + meal);
   const mealList = document.querySelector('.mealList');
   data.meals.forEach(meal => {
+    // const obj = {
+    //   meal: meal.strMeal,
+    //   id: meal.idMeal
+    // }
     col += `
           <div class="meals col-xl-4 col-lg-6 col-sm-12  m-0">
             <div class="card mealCard" style="width: 18rem;">
+              <a class="heartBtn" onclick="addFavourate('${meal.strMeal}','${meal.idMeal}','${meal.strMealThumb}') "><i class="fa-solid fa-heart heartIcon"></i></a> 
               <img src="${meal.strMealThumb}" style="height:100%" class="card-img-top" alt="meal">
               <div class="card-body">
               <h4>${meal.strMeal}</h4>
-                <button type="button" onclick='showRecipe("${meal.idMeal}")' class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <button type="button" onclick='showRecipe("${meal.idMeal}")' class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#recipe">
                 See Recipe
                 </button>
               </div>
@@ -53,6 +60,34 @@ async function renderMeal(meal) {
 
     mealList.innerHTML = col;
   })
+}
+
+const favArr = [];
+let favList = '';
+const addFavourate = (mealName, mealId, mealImg) => {
+  const favListEl = document.querySelector('.favList');
+  const obj = {
+    meal: mealName,
+    id: mealId,
+    img: mealImg
+  };
+  const found = favArr.find(element => element.id === mealId);
+  if (!found) {
+    favArr.push(obj);
+    favList += `
+    <div class="meals  col-lg-6 col-sm-12  m-0">
+      <div class="card mealCard" style="width: 18rem;">
+        <img src="${mealImg}" style="height:100%" class="card-img-top" alt="meal">
+        <div class="card-body">
+        <h4>${mealName}</h4>
+        </div>
+      </div>
+    </div>
+  `;
+  }
+
+  favListEl.innerHTML = favList;
+  console.log(favArr)
 }
 
 const showRecipe = async (mealId) => {  // show instruction in the modal
